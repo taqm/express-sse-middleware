c90ee080fdec542f5364f4d862e2c596eeb445c0
taqm <warabii.taqm@gmail.com>
2020-06-21 03:49:32 +0900

    cleanup
    - change to named export.
    - add test case (jest)

---
+++ src/handler.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from 'express'; // eslint-disable-line import/no-unresolved
+import SseProvider from './SseProvider';
+
+declare global {
+  export namespace Express {
+    export interface Response {
+      sse(param?: SseParam): SseProvider;
+    }
+  }
+}
+
+type SseParam = {
+  statusCode?: number;
+  headers?: object;
+};
+
+const handler = (
+  _: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  res.sse = (param: SseParam = {}) => {
+    res.writeHead(
+      param.statusCode ?? 200,
+      {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        Connection: 'keep-alive',
+        ...(param.headers ?? {}),
+      },
+    );
+    res.write('\n');
+
+    return new SseProvider(res);
+  };
+  next();
+};
+
+export default handler;
